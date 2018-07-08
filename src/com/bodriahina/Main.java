a1b2b8bb3da47cbd8b5f133917e37f5fc8478862
TheGreatTuna <student.backup2015@gmail.com>
2018-07-08 17:57:25 +0300

    add checking invalid data

--- src/com/bodriahina/Main.java
+++ src/com/bodriahina/Main.java
@@ -1,6 +1,7 @@
 package com.bodriahina;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
         System.out.println("input");
         Scanner in = new Scanner(System.in);
         String input = in.next();
+        while (!isValid(input)){
+            System.out.println("Not valid data. Try again");
+            System.out.println("input");
+            input = in.next();
+        }
         System.out.println("output");
         System.out.println(Tralfamadorian.convertToLong(input));
+    }
+
+    public static boolean isValid(String input){
+        return Pattern.compile("([αβγδεζηθι](\\||\\+x*|\\+\\+x*|x+))+")
+                .matcher(input)
+                .matches();
     }
 }
